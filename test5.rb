07c3a8d24be2b54161cd4529b524d2d341cb180d
wubang <wolfwubang@gmail.com>
2014-11-30 00:26:26 +0800

    ruby 变量

---
+++ test5.rb
@@ -0,0 +1,6 @@
+puts 2**4
+puts 16**0.25
+puts "Hello World #{2**4}"
+name="wubang"
+puts name
+puts "#{name}ok"
---
+++ test7.rb
@@ -0,0 +1,4 @@
+ary=["Hello","my name is wubang","i am 21 years old"]
+ary.each do |i|
+  puts i
+end
