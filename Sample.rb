eb27248b945478a66c454629acda1fc8bcaace61
wubang <wolfwubang@gmail.com>
2014-11-30 22:03:38 +0800

    operate class

---
+++ Sample.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+class Sample
+  def hello
+    #statement1
+    puts "hello,statement1"
+    #statement2
+    puts "hello,statement2"
+		#statement3
+		puts "hello"
+end
---
+++ Vehicle.rb
@@ -0,0 +1,16 @@
+Class Vehicle{
+  Number no_of_wheels
+  Number horesepower
+  Characters type_of_tank
+  Number Capacity
+  
+  Function speeding{
+    
+  }
+  Function driging{
+  
+  }
+  Function halting{
+
+  }
+}
