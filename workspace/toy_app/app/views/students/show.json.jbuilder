651263beafef5d778786ec005cbae0ea83698a37
wubang <wolfwubang@gmail.com>
2014-12-10 21:34:32 +0800

    finish student and init MicRopost

---
+++ workspace/toy_app/app/views/students/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @student, :id, :name, :pass, :created_at, :updated_at
--- workspace/toy_app/db/schema.rb
+++ workspace/toy_app/db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210123759) do
+ActiveRecord::Schema.define(version: 20141210133313) do
+
+  create_table "microposts", force: true do |t|
+    t.text     "context"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.string   "pass"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
