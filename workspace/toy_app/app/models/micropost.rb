3e68fba3f66ea9dcb6c19957f9fec2f12bcf2780
wubang <wolfwubang@gmail.com>
2014-12-12 10:49:35 +0800

    finish user validate

--- workspace/toy_app/app/models/micropost.rb
+++ workspace/toy_app/app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :context, length:{maximum:140}
-  validates :context, length:{minimum:1}
+  validates :context, presence:true
 end
