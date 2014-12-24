041414456ce02ac4bcf220a8c2d0114eb6803562
wubang <wolfwubang@gmail.com>
2014-12-24 17:33:13 +0800

    add boy function

--- lessons/girls_I_loved/app/controllers/boys_controller.rb
+++ lessons/girls_I_loved/app/controllers/boys_controller.rb
@@ -1,5 +1,46 @@
-class BoysController < ApplicationControlleri
+class BoysController < ApplicationController
+  before_action :find_by_id ,only:[:show, :edit, :update, :destroy]
+
   def index
     @boys = Boy.all
+  end
+
+  def new
+    @boy = Boy.new
+  end
+
+  def create
+    @boy = Boy.new(boy_param)
+    @boy.save
+    redirect_to @boy
+  end
+
+  def show
+    #@boy = Boy.find(params[:id])
+  end
+
+  def edit
+    #@boy = Boy.find(params[:id])
+  end
+
+  def update
+    #@boy = Boy.find(params[:id])
+    @boy.update(boy_param)
+    redirect_to boys_path
+  end
+
+  def destroy
+    #@boy =Boy.find(params[:id])
+    @boy.destroy
+    redirect_to boys_path
+  end
+
+  private
+  def boy_param
+    params.require(:boy).permit(:name, :number)
+  end
+
+  def find_by_id
+    @boy = Boy.find(params[:id])
   end
 end
